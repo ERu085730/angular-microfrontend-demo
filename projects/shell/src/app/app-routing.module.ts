d166df74184e0eda5565a95190701250790df3f5
Louie <eru085730@gmail.com>
2022-10-07 15:08:24 +0800

    fix: 修改route引入配置

--- projects/shell/src/app/app-routing.module.ts
+++ projects/shell/src/app/app-routing.module.ts
@@ -1,10 +1,15 @@
+import { loadRemoteModule } from '@angular-architects/module-federation';
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
 const routes: Routes = [
   {
     path: 'test',
-    loadChildren: () => import('page1/Module').then(m => m.TestModule)
+    loadChildren: () => loadRemoteModule({
+      type: 'module',
+      remoteEntry: "http://localhost:4201/remoteEntry.js",
+      exposedModule: './Module'
+    }).then(m => m.TestModule)
   }
 ];
 
--- projects/shell/src/decl.d.ts
+++
@@ -1,1 +0,0 @@
-declare module 'page1/Module'
